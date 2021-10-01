4d6193c1e624fbee53530fde673ee5b45bf20452
Jared Upton <jalupto@gmail.com>
2021-09-30 20:42:43 -0400

    change db name

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_URL,
+    process.env.DATABASE_URL,
     {
         dialect: "postgres",
         ssl: process.env.ENVIRONMENT === 'production',
