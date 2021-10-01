e8bdd7dc600574da11227ed4f334c8e4ef2d1814
Jared Upton <jalupto@gmail.com>
2021-09-30 23:05:55 -0400

    replace ssl and environment

--- db.js
+++ db.js
@@ -7,13 +7,13 @@ const sequelize = new Sequelize(
     process.env.DATABASE_URL,
     {
         dialect: "postgres",
-        // ssl: process.env.ENVIRONMENT === 'production',
-        // dialectOptions: {
-        //     ssl: {
-        //         require: true,
-        //         rejectUnauthorized: false
-        //     }
-        // }
+        ssl: process.env.ENVIRONMENT === 'production',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 )
 
